0f3084db0f2581e452beef0caa91b40db4c13455
alexlag <bo4onok@gmail.com>
2017-04-27 12:15:47 +0300

    history api added

--- server/api.js
+++ server/api.js
@@ -5,10 +5,9 @@ const Router = require('express').Router;
 
 const router = new Router();
 
-router.get('/today', async (req, res) => {
-  const today = getDate();
+async function getMenu({ day, month, year }) {
   const { Dishes: data = [] } = await db.Menu.findOne({
-    where: today,
+    where: { day, month, year },
     order: [['counter', 'DESC']],
     attributes: ['counter'],
     include: [{
@@ -17,13 +16,42 @@ router.get('/today', async (req, res) => {
       attributes: ['id', 'name', 'type', 'price'],
     }],
   });
-  res.send(data.map((dish) => ({
+  return data.map((dish) => ({
     id: dish.id,
     name: dish.name,
     price: dish.price,
     type: dish.type,
     complex: dish.MenuDish.complex,
-  })));
+  }));
+}
+
+router.get('/today', async (req, res) => {
+  const today = getDate();
+  try {
+    res.send(await getMenu(today));
+  } catch (e) {
+    res.send(404, []);
+  }
+});
+
+router.get('/history', async (req, res) => {
+  try {
+    const all = await db.Menu.findAll({
+      group: ['year', 'month', 'day'],
+      attributes: ['year', 'month', 'day'],
+    });
+    res.send(all);
+  } catch (e) {
+    res.send(500, e);
+  }
+});
+
+router.get('/history/:year/:month/:day', async (req, res) => {
+  try {
+    res.send(await getMenu(req.params));
+  } catch (e) {
+    res.send(404, []);
+  }
 });
 
 module.exports = router;
